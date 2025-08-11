069df61a2babb72860a612f7446ec6c51e1264d6
Jenarthanan <jenarthanan369@gmail.com>
2025-08-11 16:31:26 +0530

    execute eval()

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-'use strict'
+// 'use strict'
 const num_Input = document.getElementById('numInput');
+const _clear_key = document.getElementById('clear-key');
 let expression = "";
 
 document.querySelectorAll("button").forEach(btn => {
     btn.addEventListener("click", () => {
-
+        if(btn.value === "AC"){
+            expression = "";
+            num_Input.textContent = expression;
+        }
+        else if(btn.value === "="){
+            expression = eval(expression);
+            num_Input.textContent = expression;
+        }
+        else{     
         expression += btn.value; //add clicked value
         num_Input.textContent = expression;
+        }
     });
 });
